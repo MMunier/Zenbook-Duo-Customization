cfae9519287fb3e58a2048218031aea700862b7f
MMunier <marc@geomaps.de>
2022-12-23 16:59:03 +0100

    Try fix the remapping-script

--- remap-touchscreen.sh
+++ remap-touchscreen.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
-active_users=$(users)
 echo "hi" >> /tmp/debug
 remap() {
+    
+    export XAUTHORITY="/home/pleb/.Xauthority"
+    # active_display_count=$(xrandr -q | grep -v disconnected | grep connected | wc -l );
+    # old_count=$active_display_count;
+    # echo "$active_display_count" >> "/tmp/debug"
+    # while [[ $active_display_count -eq $old_count ]]; do
+    #     sleep "$1"
+    #     active_display_count=$(xrandr -q | grep -v disconnected | grep connected | wc -l );  
+    #    echo "$active_display_count" >> "/tmp/debug"
+    # done	    
     sleep "$1"
-    for user in $active_users
-    do
-        export XAUTHORITY="/home/$user/.Xauthority"
-        xinput map-to-output "ELAN9008:00 04F3:2D55" "eDP1" >> /tmp/debug
-        xinput map-to-output "ELAN9009:00 04F3:2C1B" "DP3" >> /tmp/debug
-    done
+    # echo "$active_display_count" >> "/tmp/debug"
+    echo "Remapping" >> "/tmp/debug"
+    xinput map-to-output "ELAN9008:00 04F3:2D55" "eDP1" >> /tmp/debug
+    xinput map-to-output "ELAN9009:00 04F3:2C1B" "DP3" >> /tmp/debug
 }
 
 remap 1 &
